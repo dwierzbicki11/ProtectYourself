ac81714a5e0195ab8adc552f9399b2aac297632d
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2024-03-11 19:08:44 +0100

    cos

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -24,5 +24,8 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+        private void Send_OnClick(object sender, RoutedEventArgs e) {
+            
+        }
     }
 }
