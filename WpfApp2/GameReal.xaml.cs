31dd707bf6dde4f4f21d30855432e6177be5d3b8
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2024-04-02 00:02:58 +0200

    update

--- WpfApp2/GameReal.xaml.cs
+++ WpfApp2/GameReal.xaml.cs
@@ -1,78 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Timers;
 using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Threading;
-using WpfApp2.script.simulated.protect.boost;
 
 namespace WpfApp2
 {
-    /// <summary>
-    /// Logika interakcji dla klasy GameReal.xaml
-    /// </summary>
-
     public partial class GameReal : Window
     {
-        //timers
-        DispatcherTimer timerVirus;
-        DispatcherTimer timerTrojan;
-        DispatcherTimer timerRansomware;
-        DispatcherTimer timerErrors;
-        DispatcherTimer timer;
-        DispatcherTimer timerUpdate;
+        // Timers
+        private Timer timerVirus;
+        private Timer timerTrojan;
+        private Timer timerRansomware;
+        private Timer timerErrors;
+        private Timer timerUpdate;
 
-        //object random
-        Random rnd = new Random();
+        // Obiekt Random
+        private Random rnd = new Random();
 
-        //Integers
-        public int prog = 10;
+        // Liczby całkowite
+        private int prog = 10;
         public int iloscVirus { get; set; } = 0;
         public int iloscTrojan { get; set; } = 0;
         public int iloscRansomware { get; set; } = 0;
         public int iloscErrors { get; set; } = 0;
 
-        int ileRazyUpdate = 0;
-        public int gold =0;
+        private int ileRazyUpdate = 0;
+        public int gold { get; set; } = 0;
         public int timeVirus, timeTrojan, timeRansomware, timeErrors;
-        int errors;
-        
-        //Constructor
+        private int errors;
+
+        // Konstruktor
         public GameReal()
         {
             InitializeComponent();
             timeVirus = rnd.Next(1, 3);
-            timerVirus = new DispatcherTimer();
-            timerVirus.Interval = TimeSpan.FromSeconds(timeVirus);
-            timerVirus.Tick += Timer_Tick;
+            timerVirus = new Timer(timeVirus * 1000);
+            timerVirus.Elapsed += TimerVirus_Elapsed;
+            timerVirus.AutoReset = true;
             timerVirus.Start();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(0.1);
-            timer.Tick += Timer5_Tick;
-            timer.Start();
-
             timeTrojan = rnd.Next(1, 5);
-            timerTrojan = new DispatcherTimer();
-            timerTrojan.Interval = TimeSpan.FromSeconds(timeTrojan);
-            timerTrojan.Tick += Timer2_Tick;
+            timerTrojan = new Timer(timeTrojan * 1000);
+            timerTrojan.Elapsed += TimerTrojan_Elapsed;
+            timerTrojan.AutoReset = true;
             timerTrojan.Start();
 
             timeRansomware = rnd.Next(3, 8);
-            timerRansomware = new DispatcherTimer();
-            timerRansomware.Interval = TimeSpan.FromSeconds(timeRansomware);
-            timerRansomware.Tick += Timer3_Tick;
+            timerRansomware = new Timer(timeRansomware * 1000);
+            timerRansomware.Elapsed += TimerRansomware_Elapsed;
+            timerRansomware.AutoReset = true;
             timerRansomware.Start();
 
-
-            timeErrors = rnd.Next(5, 15);
-            timerErrors = new DispatcherTimer();
-            timerErrors.Interval = TimeSpan.FromSeconds(timeErrors);
-            timerErrors.Tick += Timer4_Tick;
+            timeErrors = rnd.Next(1, 3);
+            timerErrors = new Timer(timeErrors * 1000);
+            timerErrors.Elapsed += TimerErrors_Elapsed;
+            timerErrors.AutoReset = true;
             timerErrors.Start();
 
-            timerUpdate = new DispatcherTimer();
-            timerUpdate.Interval = TimeSpan.FromSeconds(0.1);
-            timerUpdate.Tick += TimerUpdate_Tick;
+            timerUpdate = new Timer(100); // 0.1 sekundy w milisekundach
+            timerUpdate.Elapsed += TimerUpdate_Elapsed;
             timerUpdate.Start();
 
             virus.Content = iloscVirus;
@@ -80,76 +65,96 @@ namespace WpfApp2
             ransomware.Content = iloscRansomware;
             error.Content = iloscErrors;
         }
-        private void Timer_Tick(object sender, EventArgs e)
+
+        private void TimerVirus_Elapsed(object sender, ElapsedEventArgs e)
         {
-            // Call your functions here to add threats
-            AddVirus();
-        }
-        private void TimerUpdate_Tick(object sender, EventArgs e)
-        {
-            Update();
-        }
-        private void Timer2_Tick(object sender, EventArgs e)
-        {
-            // Call your functions here to add threats
-            AddTrojan();
-        }
-        private void Timer3_Tick(object sender, EventArgs e)
-        {
-            // Call your functions here to add threats
-            AddRansomware();
-        }
-        private void Timer4_Tick(object sender, EventArgs e)
-        {
-            // Call your functions here to add threats
-            AddErrors();
-        }
-        private void Timer5_Tick(object sender, EventArgs e)
-        {
-            errors = Convert.ToInt32(virus.Content) + Convert.ToInt32(trojan.Content) + Convert.ToInt32(ransomware.Content) + Convert.ToInt32(error.Content);
-            liczbaZagrozen.Content = errors.ToString();
-        }
-        
-        private void Update() {
-            if (errors > prog)
+            Dispatcher.Invoke(() =>
             {
-                updates.IsEnabled = true;
-                prog += rnd.Next(10, 30);
-            }
-            else updates.IsEnabled = false;
+                DodajVirusa();
+            });
         }
-        private void AddVirus()
+
+        private void TimerTrojan_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DodajTrojana();
+            });
+        }
+
+        private void TimerRansomware_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DodajRansomware();
+            });
+        }
+
+        private void TimerErrors_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                DodajBledy();
+            });
+        }
+
+        private void TimerUpdate_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Aktualizuj();
+            });
+        }
+
+        private void Aktualizuj()
+        {
+            errors = iloscVirus + iloscTrojan + iloscRansomware + iloscErrors;
+
+            if (prog < errors)
+            {
+                updates.IsEnabled = true; // Ustaw przycisk na aktywny
+            }
+            else
+            {
+                updates.IsEnabled = false; // Ustaw przycisk na nieaktywny
+                prog = rnd.Next(10, 31);
+            }
+        }
+
+
+        private void DodajVirusa()
         {
             iloscVirus++;
-            virus.Content = iloscVirus; // Update the virus count
+            virus.Content = iloscVirus;
         }
 
-        private void AddTrojan()
+        private void DodajTrojana()
         {
             iloscTrojan++;
-            trojan.Content = iloscTrojan; // Update the trojan count
+            trojan.Content = iloscTrojan;
         }
 
-        private void AddRansomware()
+        private void DodajRansomware()
         {
             iloscRansomware++;
-            ransomware.Content = iloscRansomware; // Update the ransomware count
+            ransomware.Content = iloscRansomware;
         }
 
-        private void AddErrors()
+
+        private void DodajBledy()
         {
             iloscErrors++;
-            error.Content = iloscErrors; // Update the errors count
+            error.Content = iloscErrors;
         }
 
         private void updates_Click(object sender, RoutedEventArgs e)
         {
             ileRazyUpdate++;
-            gold+=rnd.Next(1,5);
-            iloscErrors-= (int)(iloscErrors * 0.8);
-            iloscRansomware-= (int)(iloscRansomware * 0.8);
-            iloscTrojan-= (int)(iloscTrojan * 0.8);
-            iloscVirus-=(int)(iloscVirus*0.8);
+            gold += rnd.Next(1, 5);
+            iloscErrors -= (int)(iloscErrors * 0.8);
+            iloscRansomware -= (int)(iloscRansomware * 0.8);
+            iloscTrojan -= (int)(iloscTrojan * 0.8);
+            iloscVirus -= (int)(iloscVirus * 0.8);
             if (iloscVirus < 0) iloscVirus = 0;
             if (iloscTrojan < 0) iloscTrojan = 0;
             if (iloscRansomware < 0) iloscRansomware = 0;
@@ -158,7 +163,6 @@ namespace WpfApp2
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
             var shop = new shop();
             shop.Show();
         }
